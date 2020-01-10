b567d30a2b1fe638d64b5ab0854b5717af2d2762
saida_el_hajjem <Saidahajjem2017@users.noreply.github.com>
2020-01-10 22:58:30 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './home/nav-bar/nav-bar.component';
+import { SideBarComponent } from './home/side-bar/side-bar.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { ContainerComponent } from './home/container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    SideBarComponent,
+    FooterComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
